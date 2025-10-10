8d6f9ab10f3a315f113ba1b7b1ce4d20f5578afe
Noxtimo <>
2025-10-10 14:41:43 +0100

    feat(list): populate ModuleListScreen with modules data; add styles and Pressable with selection alert

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,11 +1,42 @@
-import { Text } from 'react-native';
+import { Text, ScrollView, StyleSheet, Pressable } from 'react-native';
 import Screen from '../layouts/Screen';
+import { modules as initialModules } from '../../data/modules';
 
 export default function App() {
+  // Initialisations ---------------------
+  const modules = initialModules;
+  const handleSelect = (module) => {
+    alert(`${module.ModuleCode} ${module.ModuleName}`);
+  };
+
   return (
     <Screen>
-      <Text>List</Text>
+      <ScrollView style={styles.container}>
+        {modules.map((module) => (
+          <Pressable
+            key={module.ModuleCode}
+            style={styles.item}
+            onPress={() => handleSelect(module)}
+          >
+            <Text style={styles.text}>
+              {module.ModuleCode} {module.ModuleName}
+            </Text>
+          </Pressable>
+        ))}
+      </ScrollView>
     </Screen>
   );
 }
  
+const styles = StyleSheet.create({
+  container: {},
+  item: {
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: 'lightgray',
+  },
+  text: {
+    fontSize: 16,
+  },
+});
+
