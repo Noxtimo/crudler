ea9197805520c26956817487d4a2a557f6439e65
Noxtimo <>
2025-10-10 14:58:32 +0100

    feat: Add delete functionality and state management

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,17 +1,22 @@
+import { useState } from 'react';
 import Screen from '../layouts/Screen';
 import { modules as initialModules } from '../../data/modules';
 import ModuleList from '../entity/modules/ModuleList';
 
 export default function App() {
   // Initialisations ---------------------
-  const modules = initialModules;
-  const handleSelect = (module) => {
-    alert(`${module.name}`);
+  const [modules, setModules] = useState(initialModules);
+
+  // Handlers ----------------------------
+  const handleDelete = (module) => {
+    const newModules = modules.filter((item) => item.id !== module.id);
+    setModules(newModules);
+    console.log(`After deleting ${module.name}, modules length is ${newModules.length}`);
   };
 
   return (
     <Screen>
-      <ModuleList modules={modules} onSelect={handleSelect} />
+      <ModuleList modules={modules} onSelect={handleDelete} />
     </Screen>
   );
 }
